30140c9ac05500e14a17cb63273e5e267f4c8af2
Pasi Välkkynen <pasi.valkkynen@iki.fi>
2026-04-22 23:51:56 +0300

    Add --verbose flag

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ Personal spending data input program
 
     ./run.sh
 
+Use `--verbose` to enable debug output:
+
+    ./run.sh --verbose
+
 ## Testing
 
     pip install -r dev-requirements.txt
--- spend/main.py
+++ spend/main.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 
 from .db import get_connection, init_db
@@ -5,7 +6,12 @@ from .shell import SpendShell
 
 
 def main():
-    logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.WARNING)
+    parser = argparse.ArgumentParser(description="Personal spending tracker")
+    parser.add_argument("--verbose", action="store_true", help="Enable debug output")
+    args = parser.parse_args()
+
+    level = logging.DEBUG if args.verbose else logging.WARNING
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=level)
     conn = get_connection("spend.db")
     with conn:
         init_db(conn)
