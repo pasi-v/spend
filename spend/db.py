744ef5501848aa2ad6f5af0145cad0233d82a454
Pasi Välkkynen <pasi.valkkynen@iki.fi>
2026-04-25 13:30:12 +0300

    Add type hints to db.py

--- spend/db.py
+++ spend/db.py
@@ -4,14 +4,14 @@ from . import products
 from . import vouchers
 from . import stores
 
-def get_connection(dbname: str="spend.db"):
+def get_connection(dbname: str="spend.db") -> sqlite3.Connection:
     conn = sqlite3.connect(dbname)
     conn.execute("PRAGMA foreign_keys = 1")
     conn.row_factory = sqlite3.Row
     return conn
 
 
-def init_db(conn):
+def init_db(conn: sqlite3.Connection) -> None:
     conn.executescript(producers.schema())
     conn.executescript(products.schema())
     conn.executescript(stores.schema())
